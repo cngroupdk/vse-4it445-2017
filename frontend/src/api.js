b68af600446dfaebc0b0c5d549a2ad223c1dfd47
Jakub Řičař <jakub.ricar@gmail.com>
2017-10-16 15:02:12 +0200

    Frontend - use .env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
-export const API_URL = 'http://dev.backend.USERNAME.vse.handson.pro';
-//export const API_URL = 'http://localhost:3000';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
